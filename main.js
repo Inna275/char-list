cd2161fe3b673822ea9eb958335babff67e8a324
Inna Denysiuk <innadenysiuk275@gmail.com>
2025-03-28 18:30:53 +0200

    Add length method

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import { ArrayList } from './src/list.js'
 
 const list = new ArrayList();
+
+console.log("Length:", list.length());
--- src/list.js
+++ src/list.js
@@ -2,6 +2,10 @@ class ArrayList {
   constructor() {
     this.elements = [];
   }
+
+  length() {
+    return this.elements.length;
+  }
 }
 
 export { ArrayList };
